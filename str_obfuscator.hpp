bd768e9a8e304f7787db6f39b63a257fc320c127
urShadow <videnex@gmail.com>
2017-06-28 20:08:52 +0300

    Reformat code

--- str_obfuscator.hpp
+++ str_obfuscator.hpp
@@ -1,13 +1,10 @@
 #ifndef STR_OBFUSCATOR_HPP_
 #define STR_OBFUSCATOR_HPP_
 
-namespace detail
-{
+namespace detail {
 	template<std::size_t index>
-	struct encryptor
-	{
-		static constexpr void encrypt(char *dest, const char *str, char key)
-		{
+	struct encryptor {
+		static constexpr void encrypt(char *dest, const char *str, char key) {
 			dest[index] = str[index] ^ key;
 
 			encryptor<index - 1>::encrypt(dest, str, key);
@@ -15,37 +12,31 @@ namespace detail
 	};
 
 	template<>
-	struct encryptor<0>
-	{
-		static constexpr void encrypt(char *dest, const char *str, char key)
-		{
+	struct encryptor<0> {
+		static constexpr void encrypt(char *dest, const char *str, char key) {
 			dest[0] = str[0] ^ key;
 		}
 	};
 };
 
-class cryptor
-{
+class cryptor {
 public:
-
 	template<std::size_t S>
-	class string_encryptor
-	{
+	class string_encryptor {
 	public:
-
 		constexpr string_encryptor(const char str[S], char key) :
-			_buffer{}, _decrypted{ false }, _key{ key }
-		{
+			_buffer{}, _decrypted{false}, _key{key} {
 			detail::encryptor<S - 1>::encrypt(_buffer, str, key);
 		}
 
-		const char *decrypt(void) const
-		{
-			if (_decrypted)
+		const char *decrypt(void) const {
+			if (_decrypted) {
 				return _buffer;
+			}
 
-			for (auto &c : _buffer)
+			for (auto &c : _buffer) {
 				c ^= _key;
+			}
 
 			_decrypted = true;
 
@@ -53,15 +44,13 @@ public:
 		}
 
 	private:
-
 		mutable char _buffer[S];
 		mutable bool _decrypted;
 		const char _key;
 	};
 
 	template<std::size_t S>
-	static constexpr auto create(const char(&str)[S])
-	{
+	static constexpr auto create(const char(&str)[S]) {
 		return string_encryptor<S>{ str, S };
 	}
 };
