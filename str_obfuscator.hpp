9bfc18b4165621807de74e37164b85fa318ceed2
katursis <videnex@gmail.com>
2021-08-07 22:42:11 +0000

    Update indents

--- str_obfuscator.hpp
+++ str_obfuscator.hpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 urShadow
+ * Copyright (c) 2016 katursis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,8 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -26,68 +26,73 @@
 #define STR_OBFUSCATOR_HPP_
 
 #ifdef _MSC_VER
-	#define forceinline __forceinline
+#define forceinline __forceinline
 #else
 #ifdef __GNUC__
-	#define forceinline __attribute__((always_inline))
+#define forceinline __attribute__((always_inline))
 #endif
 #endif
 
 namespace detail {
-	template<std::size_t index>
-	struct encryptor {
-		forceinline static constexpr void encrypt(char *dest, const char *str, char key) {
-			dest[index] = str[index] ^ key;
+template <std::size_t index>
+struct encryptor {
+  forceinline static constexpr void encrypt(char *dest, const char *str,
+                                            char key) {
+    dest[index] = str[index] ^ key;
 
-			encryptor<index - 1>::encrypt(dest, str, key);
-		}
-	};
-
-	template<>
-	struct encryptor<0> {
-		forceinline static constexpr void encrypt(char *dest, const char *str, char key) {
-			dest[0] = str[0] ^ key;
-		}
-	};
+    encryptor<index - 1>::encrypt(dest, str, key);
+  }
 };
+
+template <>
+struct encryptor<0> {
+  forceinline static constexpr void encrypt(char *dest, const char *str,
+                                            char key) {
+    dest[0] = str[0] ^ key;
+  }
+};
+};  // namespace detail
 
 class cryptor {
-public:
-	template<std::size_t S>
-	class string_encryptor {
-	public:
-		constexpr string_encryptor(const char str[S], int key) :
-			_buffer{}, _decrypted{false}, _key{ static_cast<const char>(key % 255) } {
-			detail::encryptor<S - 1>::encrypt(_buffer, str, _key);
-		}
+ public:
+  template <std::size_t S>
+  class string_encryptor {
+   public:
+    constexpr string_encryptor(const char str[S], int key)
+        : _buffer{},
+          _decrypted{false},
+          _key{static_cast<const char>(key % 255)} {
+      detail::encryptor<S - 1>::encrypt(_buffer, str, _key);
+    }
 
-		#ifdef __GNUC__
-		__attribute__((noinline))
-		#endif
-		const char *decrypt() const {
-			if (_decrypted) {
-				return _buffer;
-			}
+#ifdef __GNUC__
+    __attribute__((noinline))
+#endif
+    const char *
+    decrypt() const {
+      if (_decrypted) {
+        return _buffer;
+      }
 
-			for (auto &c : _buffer) {
-				c ^= _key;
-			}
+      for (auto &c : _buffer) {
+        c ^= _key;
+      }
 
-			_decrypted = true;
+      _decrypted = true;
 
-			return _buffer;
-		}
+      return _buffer;
+    }
 
-	private:
-		mutable char _buffer[S];
-		mutable bool _decrypted;
-		const char _key;
-	};
+   private:
+    mutable char _buffer[S];
+    mutable bool _decrypted;
+    const char _key;
+  };
 
-	template<std::size_t S>
-	static constexpr auto create(const char(&str)[S]) {
-		return string_encryptor<S>{ str, S };
-	}
+  template <std::size_t S>
+  static constexpr auto create(const char (&str)[S]) {
+    return string_encryptor<S>{str, S};
+  }
 };
 
-#endif // STR_OBFUSCATOR_HPP_
+#endif  // STR_OBFUSCATOR_HPP_
